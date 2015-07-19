01f2865415cec263783a716e35a790bfc8a9ae53
Marty Lamb <mlamb@martiansoftware.com>
2015-07-19 10:27:49 -0400

    added authentication

---
+++ src/main/java/com/martiansoftware/boom/auth/User.java
@@ -0,0 +1,65 @@
+package com.martiansoftware.boom.auth;
+
+import java.util.Set;
+
+/**
+ *
+ * @author mlamb
+ */
+public class User {
+
+    static final String SESSION_KEY = "BOOM_USER";
+
+    protected final String _username;
+    private final Set _permissions = new java.util.HashSet();
+    
+    /**
+     * Creates a new User with the specified username.  The username should
+     * be canonical, i.e. in your system's preferred format.  Depending upon
+     * your needs, this might mean e.g. converting an email address to lowercase
+     * and trimming leading/trailing whitespace.
+     * 
+     * @param username the CANONICAL username for this user.
+     */
+    public User(String username) {
+        _username = username;
+    }
+    
+    /**
+     * Returns the CANONICAL username for this user.  Depending upon your needs,
+     * this might mean e.g. converting an email address to lowercase and trimming
+     * leading/trailing whitespace.
+     * 
+     * @return the CANONICAL username for this user.
+     */
+    public String username() {
+        return _username;
+    }
+    
+    /**
+     * Indicates whether this user has the specified permission.  Permissions
+     * may be any object that provides hashCode() and equals(), which are used
+     * in making this determination.
+     * 
+     * @param perm the permission we're checking for
+     * @return true iff this user has the specified permission
+     */
+    public final boolean hasPermission(Object perm) {
+        return _permissions.contains(perm);
+    }
+    
+    /**
+     * Grants this user the specified permission.  Permissions may be any object
+     * that provides hashCode() and equals(), which are used in determining
+     * whether a user has a given permission.
+     * 
+     * @param perm the permission to add
+     * @return this user
+     */
+    protected final User addPermission(Object perm) {
+        _permissions.add(perm);
+        return this;
+    }
+    
+    // TODO: public static final Optional<User> current();
+}
