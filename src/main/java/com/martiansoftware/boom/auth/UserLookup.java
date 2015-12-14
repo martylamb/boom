7b799633d16758a3568764b2217f8eed54b4e67b
Marty Lamb <mlamb@martiansoftware.com>
2015-12-13 20:14:51 -0500

    reworked authentication API... now simpler and cleaner, and capable of handling changes in credentials during active sessions

---
+++ src/main/java/com/martiansoftware/boom/auth/UserLookup.java
@@ -0,0 +1,24 @@
+package com.martiansoftware.boom.auth;
+
+import java.util.Optional;
+
+/**
+ * Provides a means of looking up User objects by their username ("login", etc.).
+ * 
+ * Implementations MUST accept any form of the username supplied by the user
+ * during the login process (mixed upper and lower case, leading and trailing
+ * spaces, etc.)
+ * 
+ * @author mlamb
+ */
+public interface UserLookup {
+
+    /**
+     * Returns the requested User, or Optional.EMPTY if no such user exists.
+     * @param username the username (or "login") of the requested user, possibly
+     * in a non-canonical form such as mixed upper and lower case and/or with
+     * leading/trailing spaces, etc.
+     * @return the requested User, or Optional.EMPTY if no such user exists.
+     */
+    public Optional<User> byName(String username);
+}
